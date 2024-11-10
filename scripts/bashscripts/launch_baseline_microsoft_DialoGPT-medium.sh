f7ddc724a6291118a652c1e785c37833e144f2f1
Akis Lionis <akislionis@gmail.com>
2024-11-10 12:07:14 +0100

    reorganizing

---
+++ scripts/bashscripts/launch_baseline_microsoft_DialoGPT-medium.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# DATASET_LIST=('personachat' 'qnli' 'mnli' 'sst2' 'wmt16' 'multi_woz')
+# EMBED_MODEL_LIST=('mpnet' 'sent_roberta' 'simcse_bert' 'simcse_roberta' 'sent_t5')
+# MODEL_TYPE_LIST=('RNN' 'NN')
+
+# Default values
+DATASET_LIST=('qnli')
+EMBED_MODEL_LIST=('sent_roberta')
+MODEL_TYPE_LIST=('NN')
+
+for DATASET in "${DATASET_LIST[@]}"
+do
+    for EMBED_MODEL in "${EMBED_MODEL_LIST[@]}"
+    do
+        for MODEL_TYPE in "${MODEL_TYPE_LIST[@]}"
+        do
+            echo "Processing dataset: $DATASET with embed model: $EMBED_MODEL and model type: $MODEL_TYPE   "
+            sbatch --job-name "evaluation_baseline_${DATASET}_${EMBED_MODEL}_${MODEL_TYPE}" ${HOME}/GEIA/scripts/jobs/baseline_train_eval.job --model_dir baseline_weights/DialoGPT-medium --dataset ${DATASET} --data_type test --embed_model ${EMBED_MODEL} --model_type ${MODEL_TYPE}
+        done
+    done
+done
